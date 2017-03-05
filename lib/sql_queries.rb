c4f1021608d9c9ec7a8d24fdcb3211a060f0c017
thomashines <thomashines@gmail.com>
2017-03-04 21:35:56 -0500

    Done.

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,8 +42,6 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
   ORDER BY SUM(pledges.amount);"
 end
 
-
-
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
   "SELECT projects.category,
   pledges.amount
