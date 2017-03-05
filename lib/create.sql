c5a790536a32bdd4bb2e476d0ca2abac7f9bb6e4
thomashines <thomashines@gmail.com>
2017-03-04 21:33:15 -0500

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE projects (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+  );
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+  );
+
+CREATE TABLE pledges (
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+  );
+
+  -- you should have a table for
+  --
+  -- projects,
+  -- users,
+  -- pledges.
+  --
+  -- Create a schema based on the following information:
+  -- A project has a title, a category, a funding goal, a start date, and an end date.
+  --
+  -- A user has a name and an age
+  --
+  -- A pledge has an amount. It belongs to a user, and it also belongs to a project.
